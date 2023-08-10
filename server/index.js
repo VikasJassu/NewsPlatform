0d6ed5a5bb928c352b16b8b6f08cc2abe0b6d00f
Vikas Jassu <121491384+VikasJassu@users.noreply.github.com>
2023-08-11 01:14:10 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cookieParser()); 
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		origin:"https://news-platform-beta.vercel.app",
 		credentials:true,
 	})
 )
